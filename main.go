00488e2565531e908a4b3c079254ba795fa06991
Tiago Costa <tiagodpv@gmail.com>
2024-02-13 07:12:34 -0500

    add range

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+
+type Produto struct {
+	Nome string
+	Descricao string
+	Preco float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	produtos := []Produto {
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade:5}, //duas maneiras de criar um produt
+		{"Tenis", "Confortavel", 89, 3}, //duas maneiras de criar um produt
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
